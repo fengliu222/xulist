91cbdd586ce87214e625b75ad9633bba8975a638
Moe <444984@qq.com>
2013-05-27 19:04:53 +0800

    架构

---
+++ app/js/config.js
@@ -0,0 +1,18 @@
+seajs.config({
+  // 配置插件
+  plugins: ['shim'],
+  base: 'http://127.0.0.1/',
+
+  // 配置别名
+  alias: {
+    // 配置 jquery 的 shim 配置，这样我们就可以通过 require('jquery') 来获取 jQuery
+    'jquery': {
+      src: 'js/lib/jquery.js',
+      exports: 'jQuery'
+    },
+    'backbone':{
+      src: 'js/lib/backbone-min.js',
+      exports: 'backbone'
+    }
+  }
+});
